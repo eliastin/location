19670570e15567d93535d01db24378a84a11ce3d
Elias <oeliastin@gmail.com>
2019-03-05 16:49:18 +0800

    init

---
+++ location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  location
+//
+//  Created by 丁朝勇 on 4/18/18.
+//  Copyright © 2018 丁朝勇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
